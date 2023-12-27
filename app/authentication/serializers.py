1de8453519e02040696450e996884f1de10df883
Fahad Mashi <Fahadmashi@hotmail.com>
2023-12-27 17:04:38 +0300

    A- Serializer

--- app/authentication/serializers.py
+++ app/authentication/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from .models import CustomUser
+from .models import CustomUser,Cities,Semesters,Desposites,Profile
 
 class UserSerializer(serializers.ModelSerializer):
    class Meta:
@@ -18,5 +18,59 @@ class UserSerializer(serializers.ModelSerializer):
        user.save()
        return user
 
+class CitiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cities
+        fields = ['city', 'city_person']
 
-    
+    def validate(self, data):
+        unknown = set(data) - set(self.fields)
+        if unknown:
+            raise ValidationError(f"Unknown field(s): {', '.join(unknown)}")
+        return data
+
+class SemestersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Semesters
+        fields = ['semester', 'semester_person']
+
+    def validate(self, data):
+        unknown = set(data) - set(self.fields)
+        if unknown:
+            raise ValidationError(f"Unknown field(s): {', '.join(unknown)}")
+        return data
+
+class DespositesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Semesters
+        fields = ['user', 'copy','issue_date','due_date']
+    def validate(self, data):
+        unknown = set(data) - set(self.fields)
+        if unknown:
+            raise ValidationError(f"Unknown field(s): {', '.join(unknown)}")
+        return data
+
+
+class DespositesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Desposites
+        fields = ['user', 'copy','issue_date','due_date']
+
+    def validate(self, data):
+        unknown = set(data) - set(self.fields)
+        if unknown:
+            raise ValidationError(f"Unknown field(s): {', '.join(unknown)}")
+        return data
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['user', 'first_name','last_name','father_name','contact_no','identification_no',
+                  'registration_no','page_no','original_address','current_address','signature']
+
+    def validate(self, data):
+        unknown = set(data) - set(self.fields)
+        if unknown:
+            raise ValidationError(f"Unknown field(s): {', '.join(unknown)}")
+        return data
