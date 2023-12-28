32713227705c5f6e32e9da4c9995442f42810518
Fahad Mashi <Fahadmashi@hotmail.com>
2023-12-28 09:25:18 +0300

    A - News SEZ + URLS + VIEWS

--- app/news/urls.py
+++ app/news/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
-from .import views
+from rest_framework.routers import DefaultRouter
+from .views import NewsViewSet
+
+router = DefaultRouter()
+router.register(r'news', NewsViewSet, basename='news')
 
 urlpatterns = [
+    path('', include(router.urls)),
 
 ]
