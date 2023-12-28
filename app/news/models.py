b15da56509db0e1d159626698bcb475d43dc6dbc
Fahad Mashi <Fahadmashi@hotmail.com>
2023-12-28 08:12:19 +0300

    A - News Model

--- app/news/models.py
+++ app/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from book.models import Faculty
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    details = models.TextField()
+    news_reference = models.CharField(max_length=255)
+    news_title_persian = models.CharField(max_length=200)
+    news_summary_persian = models.TextField()
+    news_reference_persian = models.CharField(max_length=255)
+    date = models.DateField()
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
